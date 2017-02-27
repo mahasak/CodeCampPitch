a2198e22a3390c2bd83058cd8c3a595971c54933
Mahasak Pijittum <mahasak@gmail.com>
2017-02-27 21:54:09 +0700

    add test setup and teardown

--- python-flask/hello.py
+++ python-flask/hello.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello_template(name=None):
+    return render_template('hello.html', name=name)
 
 if __name__ == "__main__":
     app.run()
