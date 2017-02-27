1223b69b31c5a905085caa9264c0b90a0aa8a941
Mahasak Pijittum <mahasak@gmail.com>
2017-02-27 21:48:06 +0700

    add test setup and teardown

--- python-flask/test_sample_math.py
+++ python-flask/test_sample_math.py
@@ -1,7 +1,16 @@
+from nose import with_setup
 from sample_math import multiply
 
+def setup():
+    pass
+ 
+def teardown():
+    pass
+ 
+@with_setup(setup, teardown)
 def test_numbers_3_4_pass():
     assert multiply(3,4) == 12 
 
+@with_setup(setup, teardown)
 def test_numbers_3_4_failed():
     assert multiply(3,4) == 13
