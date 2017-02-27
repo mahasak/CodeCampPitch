36b006d956cef3de70a3667c314749c815d3d3c1
Mahasak Pijittum <mahasak@gmail.com>
2017-02-27 22:22:32 +0700

    refactor to comply PEP8 & pylint

--- python-flask/hello.py
+++ python-flask/hello.py
@@ -1,14 +1,25 @@
-from flask import Flask,render_template
-app = Flask(__name__, static_url_path='/static')
+"""
+Sample flask web application.
+"""
 
-@app.route("/")
+from flask import Flask, render_template
+
+APP = Flask(__name__, static_url_path='/static')
+
+@APP.route("/")
 def hello():
+    """
+    Return Hello world string.
+    """
     return "Hello World!"
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
+@APP.route('/hello/')
+@APP.route('/hello/<name>')
 def hello_template(name=None):
+    """
+    Return Hello world from template.
+    """
     return render_template('hello.html', name=name)
 
 if __name__ == "__main__":
-    app.run()
+    APP.run()
